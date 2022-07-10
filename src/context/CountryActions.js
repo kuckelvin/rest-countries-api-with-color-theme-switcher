c0a71a1e6a602710f3702531f938586d5efaf5ae
Kelvin Echenim <kuckelvin@gmail.com>
2022-07-10 22:52:11 +0100

    CSS: First Commit

---
+++ src/context/CountryActions.js
@@ -0,0 +1,10 @@
+// import CountryContext from "./CountryContext";
+// import { useContext } from "react";
+
+// export const searchCountry = (id) =>{
+//     const { results, dispatch } = useContext(CountryContext);
+
+//     let country = results.find(country => country.id === id)
+//     dispatch({type: "GET_CONCERNED_COUNTRY", payload: country})
+// }
+
